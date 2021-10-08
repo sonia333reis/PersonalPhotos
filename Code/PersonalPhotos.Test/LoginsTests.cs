a52df1a8fd0758c7bb70c1fd825b402509c2df5c
Sonia Reis <sonia333reis@gmail.com>
2021-10-07 21:13:49 -0300

    test: redirecting to login view if url is empty

--- Code/PersonalPhotos.Test/LoginsTests.cs
+++ Code/PersonalPhotos.Test/LoginsTests.cs
@@ -1,6 +1,32 @@
+using PersonalPhotos.Controllers;
+using Moq;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
 namespace PersonalPhotos.Test
 {
     public class LoginsTests
     {
+        private readonly LoginsController _controller;
+        private readonly Mock<ILogins> _logins; // Precisa ser a interface do business/services
+        private readonly Mock<IHttpContextAccessor> _acessor;
+        public LoginsTests()
+        {
+            // O moq vai criar uma instância dos attributos
+            _logins = new Mock<ILogins>();
+            _acessor = new Mock<IHttpContextAccessor>();
+            // Com meus objetos criados, eu posso passar os parâmetros necessário para o meu controller
+            _controller = new LoginsController(_logins.Object, _acessor.Object);
+        }
+
+        [Fact]
+        public void Index_GivenNoReturnUrl_ReturnLoginView()
+        {
+            var result = (_controller.Index() as ViewResult);
+            Assert.NotNull(result);
+            Assert.Equal("Login", result.ViewName, ignoreCase: true);
+        }
     }
 }

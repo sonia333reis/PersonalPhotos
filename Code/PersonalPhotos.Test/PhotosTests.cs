dde49a5bc8fbf89ba742ec98e342bf8e6cf48d03
Sonia Reis <sonia333reis@gmail.com>
2021-10-08 11:15:25 -0300

    refac: transfer generic mocks to the constructor

--- Code/PersonalPhotos.Test/PhotosTests.cs
+++ Code/PersonalPhotos.Test/PhotosTests.cs
@@ -12,6 +12,21 @@ namespace PersonalPhotos.Test
 {
     public class PhotosTests
     {
+        private readonly IFileStorage fileStore;
+        private readonly IKeyGenerator keyGen;
+        private readonly IPhotoMetaData photoMetaData;
+        private readonly IFormFile formFile;
+        private readonly PhotoUploadViewModel model;
+
+        public PhotosTests()
+        {
+            fileStore = Mock.Of<IFileStorage>();
+            keyGen = Mock.Of<IKeyGenerator>();
+            photoMetaData = Mock.Of<IPhotoMetaData>();
+            formFile = Mock.Of<IFormFile>();
+            model = Mock.Of<PhotoUploadViewModel>(x => x.File == formFile);
+        }
+
         [Fact]
         public async Task Upload_GivenFileName_ReturnsDisplayAction()
         {
@@ -20,19 +35,14 @@ namespace PersonalPhotos.Test
             session.Set("User", Encoding.UTF8.GetBytes("ab@com")); // Mockando um valor em session para não dar erro no getuser
             var context = Mock.Of<HttpContext>(x => x.Session == session);
             var acessor = Mock.Of<IHttpContextAccessor>(x => x.HttpContext == context); // Além de comparar, também atribui
-            var fileStore = Mock.Of<IFileStorage>();
-            var keyGen = Mock.Of<IKeyGenerator>();
-            var photoMetaData = Mock.Of<IPhotoMetaData>();
-            var formFile = Mock.Of<IFormFile>();
-            var model = Mock.Of<PhotoUploadViewModel>(x => x.File == formFile);
             var controller = new PhotosController(keyGen, acessor, photoMetaData, fileStore);
 
             // Fase Act -> execução das ações de teste
             var result = await controller.Upload(model) as RedirectToActionResult;
 
-
             // Assert -> comparando os resultados obtidos com os resultados esperados no teste
             Assert.Equal("Display", result.ActionName, ignoreCase: true);
         }
+
     }
 }
